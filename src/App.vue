658d8901a25b37d7340616a4121df9e90005d540
Abd-ElrahmanEid <eidabdelrahman985@gmail.com>
2023-02-27 20:50:17 +0200

    'project'

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <br><br>
+  <div class="inputs">
+    <input type="text" placeholder="Enter Your Data" v-model="data">
+
+    <button @click="addinput">
+      Create
+    </button>
+
+  </div>
+  <br>
+
+<item_box v-for="input in inputs" :key="input.content"
+:content="input.data"/>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+
+<script>
+import item_box from './components/inputs.vue';
+
+
+export default{
+    name: 'main_word',
+    data(){
+      return {
+        data: '',
+        inputs: [
+          //here pushing process
+        ]
+      }
+    },
+
+
+    components : {
+      item_box
+    },
+
+
+    methods:{
+      addinput: function(){
+        this.inputs.push({
+          data: this.data
+        });
+        this.data = ''
+      }
+      }
+    }
 </script>
 
+
+
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .inputs{
+    width: 30%;
+    height: 30%;
+    margin: auto;
+  }
+
+  input{
+    text-align: center;
+    width: 100%;
+    padding: 10px;
+  }
+
+  button{
+    margin-top: 10px;
+    padding: 5px 20px;
+    background-color: crimson;
+    color: white;
+    border: none;
+  }
+
 </style>
---
+++ src/components/inputs.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="box">
+            <h2>{{ content }}</h2>
+    </div>
+    <br>
+</template>
+
+
+
+<script>
+
+    export default{
+        name: 'item_box',
+        props: ['content']
+    }
+
+</script>
+
+
+
+
+<style>
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    .box{
+        width: 50%;
+        height: 80px;
+        border: 2px solid black;
+        text-align: center;
+        background-color: black;
+        margin: auto;
+    }
+
+    h2{
+        color: white;
+        padding-top: 15px;
+    }
+
+</style>
